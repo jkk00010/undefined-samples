575f5dbadfc0ec771cd6efd53f23ebf55349443f
Jakob Kaivo <jkk@ung.org>
2022-11-19 19:53:02 -0500

    implement

--- 6/2/2/sample-1.c
+++ 6/2/2/sample-1.c
@@ -1,1 +1,7 @@
-/*The same identifer has both internal and external linkage in the same translation unit (6.2.2). */
+/* The same identifer has both internal and external linkage in the same translation unit (6.2.2). */
+
+int main(void)
+{
+	extern int x;
+	static int x;
+}
