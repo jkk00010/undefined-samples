b1f8ddcbe00378b9f82794c414129bd20697e2cc
Jakob Kaivo <jkk@ung.org>
2022-11-21 12:08:57 -0500

    add -lpthread where needed

--- deps.sh
+++ deps.sh
@@ -6,6 +6,8 @@ printf '.POSIX:\n.SILENT:\n'
 
 for src in $(find */ -name \*.c); do
 	out=$(echo $src | sed -e 's#^/##g;s#/#.#g;s#\.c$##g;')
+	lib=''
+	if (grep -q thread $src); then lib='-lpthread '; fi
 	cat <<EOF
 
 all: \$(OUT)/${out}.log
@@ -13,7 +15,7 @@ all: \$(OUT)/${out}.log
 	mkdir -p \$(OUT)
 	rm -f \$@
 	printf ' [CC]  %s\\n' "$src"
-	-(\$(CC) \$(CFLAGS) \$< -o \$(OUT)/${out} || printf -- '--returned %d\\n' \$\$?) >> \$@ 2>> \$@
+	-(\$(CC) \$(CFLAGS) \$< ${lib}-o \$(OUT)/${out} || printf -- '--returned %d\\n' \$\$?) >> \$@ 2>> \$@
 	-if [ -x "\$(OUT)/${out}" ]; then printf ' [RUN] %s\\n' "\$(OUT)/${out}"; (printf '\\nrunning...\\n'; ./\$(OUT)/${out}; printf -- '--returned %d\\n' \$\$?) >> \$@ 2>> \$@; fi
 EOF
 done
