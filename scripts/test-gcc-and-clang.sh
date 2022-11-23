1e847720666ada009eb2ba03807c1e7f8b74cd2c
Jakob Kaivo <jkk@ung.org>
2022-11-23 14:08:07 -0500

    remove -base from baseline case

--- scripts/test-gcc-and-clang.sh
+++ scripts/test-gcc-and-clang.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-make CC=gcc CFLAGS='-std=c18' OUT=gcc-base
+make CC=gcc CFLAGS='-std=c18' OUT=gcc
 make CC=gcc CFLAGS='-std=c18 -Wall' OUT=gcc-Wall
 make CC=gcc CFLAGS='-std=c18 -Wall -Wextra' OUT=gcc-Wall-Wextra
 
-make CC=clang CFLAGS='-std=c18' OUT=clang-base
+make CC=clang CFLAGS='-std=c18' OUT=clang
 make CC=clang CFLAGS='-std=c18 -Wall' OUT=clang-Wall
 make CC=clang CFLAGS='-std=c18 -Wall -Wextra' OUT=clang-Wall-Wextra
