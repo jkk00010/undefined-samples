20fa6e90f3fe488ef6a40bc32633481785057e77
Jakob Kaivo <jkk@ung.org>
2022-11-23 14:25:37 -0500

    also check for .exe files

--- scripts/makecsv.sh
+++ scripts/makecsv.sh
@@ -13,7 +13,7 @@ for src in $(find . -name '*.c' | grep -v sample- | sort); do
 	out=$(echo $src | sed -e 's#^\./##g;s#/#.#g;s#\.c$##g')
 	printf '%s' "$(echo $out | sed -e 's/\.\([a-z_]\)/, \1/')"
 	for dir in $*; do
-		if [ ! -f "${dir}/${out}" ]; then
+		if [ ! -f "${dir}/${out}" ] && [ ! -f "${dir}/${out}.exe" ]; then
 			printf ', E'
 		elif [ $(wc -l "${dir}/${out}.log" | cut -d' ' -f1) -gt 1 ]; then
 			printf ', W'
