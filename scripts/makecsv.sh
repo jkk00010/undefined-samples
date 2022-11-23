0a1275344c13724581b4f2675e70d31dc1967f6a
Jakob Kaivo <jkk@ung.org>
2022-11-22 20:48:06 -0500

    add script to turn result dirs into a CSV

---
+++ scripts/makecsv.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+	printf 'usage: %s dir...\n' "$0"
+	exit 1
+fi
+
+printf 'section, sample'
+printf ', %s' $* | tr -d /
+printf '\n'
+
+for src in $(find . -name '*.c' | grep -v sample- | sort); do
+	out=$(echo $src | sed -e 's#^\./##g;s#/#.#g;s#\.c$##g')
+	printf '%s' "$(echo $out | sed -e 's/\.\([a-z_]\)/, \1/')"
+	for dir in $*; do
+		if [ ! -f "${dir}/${out}" ]; then
+			printf ', E'
+		elif [ $(wc -l "${dir}/${out}.log" | cut -d' ' -f1) -gt 1 ]; then
+			printf ', W'
+		else
+			printf ', I'
+		fi
+	done
+	printf '\n'
+done
