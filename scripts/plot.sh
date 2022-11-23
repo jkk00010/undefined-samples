de1651aac6eadca8acf5efce7b893df4a7ee8ee6
Jakob Kaivo <jkk@ung.org>
2022-11-23 16:37:37 -0500

    merge gnuplot commands directly into shell script

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -5,4 +5,18 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 
-gnuplot -p -e "filename='$1'" "$(dirname $0)/plot.plg"
+gnuplot -p -e "filename='$1'" -<<-EOF
+	set datafile separator ','
+	set key autotitle columnhead
+	set style data histogram
+	set style fill solid border -1
+	
+	set style line 1 lt 1 lc rgb '#440154' # dark purple
+	set style line 2 lt 1 lc rgb '#472c7a' # purple
+	set style line 3 lt 1 lc rgb '#3b518b' # blue
+	
+	set xtics rotate by -45
+	set yr [0:*]
+	
+	plot for [i=2:*] filename using i:xtic(1) title col
+EOF
