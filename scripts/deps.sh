c31a06cff55b41a4bb8184207fa8d69f277d3f26
Jakob Kaivo <jkk@ung.org>
2022-11-24 12:27:06 -0500

    drop silent, fix libraries

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -4,7 +4,6 @@ exec > Makefile
 
 cat <<EOH
 .POSIX:
-.SILENT:
 
 CC=clang
 CFLAGS=-std=c17
@@ -20,20 +19,14 @@ EOH
 for src in $(find */ -name \*.c | grep -v /sample-); do
 	out=$(echo $src | sed -e 's#^/##g;s#/#.#g;s#\.c$##g;')
 	lib=''
-	if (grep -q thread.h $src); then lib='-lpthread '; fi
-	if (grep -q -e math.h -e fenv.h $src); then lib='-lm '; fi
+	if (grep -q threads.h $src); then lib="${lib}-lpthread "; fi
+	if (grep -q -e math.h -e fenv.h $src); then lib="${lib}-lm "; fi
+
 	cat <<EOF
 
 all: \$(OUT)/${out}.log
 \$(OUT)/${out}.log: $src
-	mkdir -p \$(OUT)
-	printf ' [CC]  %s\\n' "$src"
+	@mkdir -p \$(OUT)
 	-\$(CC) \$(CFLAGS) \$< ${lib}-o \$(OUT)/${out} > \$@ 2>&1
 EOF
 done
-
-# If we want to run the generated programs, this would do it
-# But many of them hang, so let's not
-cat <<EOF > /dev/null
-	-if [ -x "\$(OUT)/${out}" ]; then printf ' [RUN] %s\\n' "\$(OUT)/${out}"; (printf '\\nrunning...\\n'; ./\$(OUT)/${out}; printf -- '--returned %d\\n' \$\$?) >> \$@ 2>> \$@; fi
-EOF
