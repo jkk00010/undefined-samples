036501e9c5018924009942972df1fc93c406fb17
Jakob Kaivo <jkk@ung.org>
2024-01-29 12:44:38 -0500

    raise signal directly

--- 7/signal-at-quick-exit.c
+++ 7/signal-at-quick-exit.c
@@ -1,11 +1,11 @@
 /* A signal is raised while the quick_exit function is executing (7.22.4.7). */
 
 #include <stdlib.h>
+#include <signal.h>
 
 void qe(void)
 {
-	int *n = NULL;
-	*n = 0;
+	raise(SIGINT);
 }
 
 int main(void)
