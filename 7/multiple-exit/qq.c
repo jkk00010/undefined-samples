476da0d74f83f791cb1a36f292e98c7660178888
Jakob Kaivo <jkk@ung.org>
2024-01-29 12:35:54 -0500

    split into four samples

---
+++ 7/multiple-exit/qq.c
@@ -0,0 +1,13 @@
+/* The program calls the exit or quick_exit function more than once, or calls both functions (7.22.4.4, 7.22.4.7). */
+#include <stdlib.h>
+
+void e2(void)
+{
+	quick_exit(2);
+}
+
+int main(void)
+{
+	at_quick_exit(e2);
+	quick_exit(1);
+}
