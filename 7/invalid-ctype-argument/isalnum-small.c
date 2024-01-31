3d2bf0864ecbf550be6ba544a22ea667b4438ae2
Jakob Kaivo <jkk@ung.org>
2024-01-31 14:26:45 -0500

    test all the ctype functions with a value slightly out of range and very out of range

---
+++ 7/invalid-ctype-argument/isalnum-small.c
@@ -0,0 +1,13 @@
+/* The value of an argument to a character handling function is neither equal to the value of EOF nor representable as an unsigned char (7.4). */
+#include <ctype.h>
+#include <limits.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int foo = UCHAR_MAX + 1;
+	if (foo == EOF) {
+		foo++;
+	}
+	return isalnum(foo);
+}
--- 7/suppressed-errno-macro.c
+++
@@ -1,8 +0,0 @@
-/* A macro definition of errno is suppressed in order to access an actual object, or the program defines an identifier with the name errno (7.5). */
-#include <errno.h>
-#undef errno
-
-int main(void)
-{
-	return (errno);
-}
