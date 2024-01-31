3c384a0c10470c501ef0ad238898af877ddbe4e8
Jakob Kaivo <jkk@ung.org>
2024-01-31 15:02:12 -0500

    add multiple ways of suppressing the macro

---
+++ 7/suppressed-math-errhandling-macro/declaration.c
@@ -0,0 +1,8 @@
+/* A macro definition of math_errhandling is suppressed or the program defines an identifier with the name math_errhandling (7.12). */
+
+extern int math_errhandling;
+
+int main(void)
+{
+	return math_errhandling;
+}
--- 7/suppressed-math-errhandling-macro/undef.c
+++ 7/suppressed-math-errhandling-macro/undef.c
@@ -1,10 +1,9 @@
 /* A macro definition of math_errhandling is suppressed or the program defines an identifier with the name math_errhandling (7.12). */
 
 #include <math.h>
-#include <stdio.h>
+#undef math_errhandling
 
 int main(void)
 {
-	int math_errhandling = 1;
-	printf("%d\n", (math_errhandling));
+	return math_errhandling;
 }
