8dfe71575657866938e4ed88e97dd6f9a5926245
Jakob Kaivo <jkk@ung.org>
2024-01-31 21:43:15 -0500

    test various ways to break %%

---
+++ 7/invalid-percent-conversion/flag-hash.c
@@ -0,0 +1,8 @@
+/* A % conversion specifier is encountered by one of the formatted input/output functions, but the complete conversion specification is not exactly %% (7.21.6.1, 7.21.6.2, 7.29.2.1, 7.29.2.2). */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%#%\n");
+}
