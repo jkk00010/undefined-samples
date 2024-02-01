2ab149a7b2f7e1038412bb81d02f41078e02bcd1
Jakob Kaivo <jkk@ung.org>
2024-01-31 21:39:43 -0500

    test various ways to break %n

---
+++ 7/invalid-n-conversion/flag-space.c
@@ -0,0 +1,9 @@
+/* An n conversion specification for one of the formatted input/output functions includes any flags, an assignment-suppressing character, a field width, or a precision (7.21.6.1, 7.21.6.2, 7.29.2.1, 7.29.2.2). */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	printf("hello % n\n", &n);
+}
