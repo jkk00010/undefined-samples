b598eb69a0e818e3d73e7ec642f3f7f2b7231b72
Jakob Kaivo <jkk@ung.org>
2024-02-06 13:17:55 -0500

    implement UB

--- 7/ungetc-at-beginning.c
+++ 7/ungetc-at-beginning.c
@@ -1,1 +1,11 @@
 /* The file position indicator for a binary stream is used after a call to the ungetc function where its value was zero before the call (7.21.7.10). */
+
+#include <stdio.h>
+
+int main(void)
+{
+	fpos_t pos;
+	FILE *f = fopen("/dev/null", "rb");
+	ungetc('a', f);
+	return fgetpos(f, &pos);
+}
