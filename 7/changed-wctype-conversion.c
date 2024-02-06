c0b5e0f85d5b39596c6bb96ff50f6f290e886d85
Jakob Kaivo <jkk@ung.org>
2024-02-06 13:09:13 -0500

    implement behaviors

--- 7/changed-wctype-conversion.c
+++ 7/changed-wctype-conversion.c
@@ -1,1 +1,11 @@
 /* The towctrans function is called using a different LC_CTYPE category from the one in effect for the call to the wctrans function that returned the description (7.30.3.2.1). */
+
+#include <locale.h>
+#include <wctype.h>
+
+int main(void)
+{
+	wctrans_t trans = wctrans("toupper");
+	setlocale(LC_ALL, "");
+	return towctrans(L'a', trans);
+}
--- 7/changed-wctype-query.c
+++ 7/changed-wctype-query.c
@@ -1,1 +1,11 @@
 /* The iswctype function is called using a different LC_CTYPE category from the one in effect for the call to the wctype function that returned the description (7.30.2.2.1). */
+
+#include <locale.h>
+#include <wctype.h>
+
+int main(void)
+{
+	wctype_t type = wctype("upper");
+	setlocale(LC_ALL, "");
+	return iswctype(L'a', type);
+}
