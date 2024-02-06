ef555d8b8d032027be066a5e2109db4137227f23
Jakob Kaivo <jkk@ung.org>
2024-02-06 13:48:27 -0500

    implement

--- 7/invalid-pointer-input.c
+++ 7/invalid-pointer-input.c
@@ -1,1 +1,9 @@
 /* The input item for a %p conversion by one of the formatted input functions is not a value converted earlier during the same program execution (7.21.6.2, 7.29.2.2). */
+
+#include <stdio.h>
+
+int main(void)
+{
+	void *ptr = NULL;
+	return sscanf("0xdeadbeef", "%p", &ptr);
+}
