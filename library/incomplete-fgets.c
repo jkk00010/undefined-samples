b73e9b81ff4dfc2ede4c9884afd29b90639cf20a
Jakob Kaivo <jkk@ung.org>
2024-03-27 16:03:56 -0400

    implement this sample

---
+++ library/incomplete-fgets.c
@@ -0,0 +1,11 @@
+/* The contents of the array supplied in a call to the fgets or fgetws function are used after a read error occurred (7.21.7.2, 7.29.3.2). */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char buf[32];
+	FILE *f = fopen("/dev/null", "w");
+	fgets(buf, sizeof(buf), f);
+	fclose(f);
+}
