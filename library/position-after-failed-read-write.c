21b5a2179ffd3077766923c25eb2bcb8f315e38a
Jakob Kaivo <jkk@ung.org>
2024-03-27 15:56:29 -0400

    write this sample

---
+++ library/position-after-failed-read-write.c
@@ -0,0 +1,14 @@
+/* The file position indicator for a stream is used after an error occurred during a call to the fread or fwrite function (7.21.8.1, 7.21.8.2). */
+
+#include <stdio.h>
+
+int main(void)
+{
+	struct {
+		int a;
+		int b;
+	} s[2];
+	FILE *f = tmpfile();
+	fread(s, sizeof(s[0]), sizeof(s) / sizeof(s[0]), f);
+	return (int)ftell(f);
+}
