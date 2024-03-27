f683938f5251ef9b9a6fd9d7bee8d9ef2cd7a7b2
Jakob Kaivo <jkk@ung.org>
2024-03-27 15:09:14 -0400

    write this sample program

--- library/unrepresentable-conversion.c
+++ library/unrepresentable-conversion.c
@@ -1,1 +1,10 @@
 /* The result of a conversion by one of the formatted input functions cannot be represented in the corresponding object, or the receiving object does not have an appropriate type (7.21.6.2, 7.29.2.2). */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 0;
+	sscanf("9999999999999999999999999999999999999999999999999999999999999999", "%d", &n);
+	return n;
+}
